33de1f00550a1cced4379e2381f7d7d26a549dde
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-19 14:16:14 -0800

    formatting + var length constructor with tests

--- GodelNumbers/GodelNumbering.java
+++ GodelNumbers/GodelNumbering.java
@@ -4,20 +4,21 @@ public class GodelNumbering {
 
     private long eNumber = 1;
 
-    public GodelNumbering(int[] sequence){
+    public GodelNumbering(int... sequence){
+        if(sequence.length == 0) sequence = new int[] {1,2,3};
         calculateE(sequence);
     }
 
-    private void calculateE(int[] sequence){
+    private void calculateE(int[] sequence) {
         long lastPrime = 1;
-        for(int i = 0; i < sequence.length; i++){
+        for (int i = 0; i < sequence.length; i++) {
             long currPrime = PrimeNumber.nextPrime(lastPrime);
-            eNumber *= Math.pow(PrimeNumber.nextPrime(lastPrime),sequence[i]);
+            eNumber *= Math.pow(PrimeNumber.nextPrime(lastPrime), sequence[i]);
             lastPrime = currPrime;
         }
     }
 
-    public long geteNumber(){
+    public long geteNumber() {
         return eNumber;
     }
 }
