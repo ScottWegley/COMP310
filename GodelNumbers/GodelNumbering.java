a2fdb7721b33911548ef8f911e63ec519acb42a7
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-19 14:04:14 -0800

    constructor to take in and loop through a sequence

--- GodelNumbers/GodelNumbering.java
+++ GodelNumbers/GodelNumbering.java
@@ -3,6 +3,13 @@ package GodelNumbers;
 public class GodelNumbering {
 
     private long eNumber = 0;
+
+    public GodelNumbering(int[] sequence){
+        for(int i = 0; i < sequence.length; i++){
+
+        }
+    }
+
     public long geteNumber(){
         return eNumber;
     }
