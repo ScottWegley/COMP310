cb27431f635979cb77489799a8ef1b9a43f17c8c
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-19 14:53:50 -0800

    formatting

--- GodelNumbers/GodelNumbering.java
+++ GodelNumbers/GodelNumbering.java
@@ -4,8 +4,10 @@ public class GodelNumbering {
 
     private long eNumber = 1;
 
-    public GodelNumbering(int... sequence){
-        if(sequence.length == 0) sequence = new int[] {1,2,3};
+    public GodelNumbering(int... sequence) {
+        if (sequence.length == 0) {
+            sequence = new int[] { 1, 2, 3 };
+        }
         calculateE(sequence);
     }
 
