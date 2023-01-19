af7004c03b430fae7b1e5a55de303a74384b7006
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-19 14:10:53 -0800

    exported calculating e to independant function

--- GodelNumbers/GodelNumbering.java
+++ GodelNumbers/GodelNumbering.java
@@ -5,6 +5,10 @@ public class GodelNumbering {
     private long eNumber = 0;
 
     public GodelNumbering(int[] sequence){
+        calculateE(sequence);
+    }
+
+    private void calculateE(int[] sequence){
         for(int i = 0; i < sequence.length; i++){
 
         }
