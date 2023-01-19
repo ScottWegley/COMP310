c71cf457315d70d533281095be834fab517ecdd9
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-19 14:11:40 -0800

    loop through sequence, grab+track primes to update E

--- GodelNumbers/GodelNumbering.java
+++ GodelNumbers/GodelNumbering.java
@@ -2,15 +2,18 @@ package GodelNumbers;
 
 public class GodelNumbering {
 
-    private long eNumber = 0;
+    private long eNumber = 1;
 
     public GodelNumbering(int[] sequence){
         calculateE(sequence);
     }
 
     private void calculateE(int[] sequence){
+        long lastPrime = 1;
         for(int i = 0; i < sequence.length; i++){
-
+            long currPrime = PrimeNumber.nextPrime(lastPrime);
+            eNumber *= Math.pow(PrimeNumber.nextPrime(lastPrime),sequence[i]);
+            lastPrime = currPrime;
         }
     }
 
