3ddc81218d3c5296588cdf915baac9a306b74e36
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-19 13:47:05 -0800

    borrowed prime code from COMP220 repo

---
+++ GodelNumbers/PrimeNumber.java
@@ -0,0 +1,39 @@
+package GodelNumbers;
+
+import java.util.ArrayList;
+
+public class PrimeNumber {
+    public long[] primeFactors(long p) {
+        ArrayList<Long> factors = new ArrayList<Long>();
+        for (long i = 2L; i <= (p); i++) {
+            if (p % i == 0) {
+                if (isPrime(i)) {
+                    factors.add(i);
+                    p = p / i;
+                }
+            }
+        }
+        long[] arr = new long[factors.size()];
+        for (int i = 0; i < factors.size(); i++) {
+            arr[i] = factors.get(i);
+        }
+        return arr;
+    }
+
+    public boolean isPrime(long p) {
+        for (long i = 2L; i <= Math.sqrt(p); i++) {
+            if (p % i == 0) {
+                return false;
+            }
+        }
+        return p != 1L;
+    }
+
+    public long nextPrime(long p) {
+        p += 1;
+        while (!isPrime(p)) {
+            p += 1;
+        }
+        return p;
+    }
+}
