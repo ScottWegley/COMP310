4e2f4ea1e8045e0d95bfd89fce363760b10e5101
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-19 16:15:40 -0800

    testing

--- GodelNumbers/TestCase.java
+++ GodelNumbers/TestCase.java
@@ -2,6 +2,21 @@ package GodelNumbers;
 
 public class TestCase {
     public static void main(String[] args) {
-        
+        int[] mySequence = new int[] {6,5,4,3,2,1 };
+        GodelNumbering testClass = new GodelNumbering(mySequence);
+
+        System.out.print("The Godel Number you got was " + testClass.geteNumber() + " from the sequence ");
+        for (int i : mySequence) {
+            System.out.print(i + " ");
+        }
+
+        if (true) {
+            System.out.print("\nBy Godel Numbering, the sequence reversed from " + testClass.geteNumber() + " is ");
+
+            long[] sourceSequence = testClass.geteSourceSequence();
+            for (long factor : sourceSequence) {
+                System.out.print(factor + " ");
+            }
+        }
     }
 }
