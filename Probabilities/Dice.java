eae2fc224d6a1ec4bd791546169f055b9098a265
Scott Wegley <scbrwegley@protonmail.ch>
2023-02-01 15:57:06 -0800

    added functionality for custom dice faces

--- Probabilities/Dice.java
+++ Probabilities/Dice.java
@@ -1,7 +1,26 @@
 public class Dice {
 
     private int sides;
+    private boolean customSidesEnabled = false;
+    private int[] customSides;
+
     public Dice(int _sides) {
         this.sides = _sides;
+    }
+
+    public void enableCustomSides() {
+        customSidesEnabled = true;
+    }
+
+    public boolean enableCustomSides(int[] inSides) {
+        if (inSides.length < this.sides) {
+            return false;
+        }
+        enableCustomSides();
+        customSides = new int[sides];
+        for (int i = 0; i < customSides.length; i++) {
+            customSides[i] = inSides[i];
+        }
+        return true;
     }
 }
