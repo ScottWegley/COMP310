7df6461700fc1733b54fa383848b4b2fccfe5e62
scottwegley <scottwegley@gmail.com>
2023-02-03 16:21:04 -0800

    testing + writeup

--- Probabilities/TestCase.java
+++ Probabilities/TestCase.java
@@ -1,5 +1,10 @@
 class TestCase {
     public static void main(String[] args) {
+        System.out.println("Problem 2");
+        Probabilities.problem2();
+        System.out.println("Problem 3");
+        Probabilities.problem3();
+        System.out.println("Problem 4");
         Probabilities.problem4();
     }
 }
