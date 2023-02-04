ce4e2f756b2abe7c4d0a1e97c2d41df85b7bf194
scottwegley <scottwegley@gmail.com>
2023-02-03 16:20:56 -0800

    condensed loops, fixed prob 4 matrix

--- Probabilities/Probabilities.java
+++ Probabilities/Probabilities.java
@@ -1,26 +1,24 @@
 
 class Probabilities {
 
-    public static int N2 = 1500000000;
-    public static int N3 = 1500000000;
-    public static int N4 = 2;
+    public static int N2 = 2000000000;
+    public static int N3 = 2000000000;
+    public static int N4 = 2000000000;
 
     public static void problem2() {
         Dice diceA = new Dice(5);
         int[] aResults = new int[5];
+        Dice diceB = new Dice(5);
+        int[] bResults = new int[9];
         for (int i = 0; i < N2; i++) {
             aResults[diceA.roll() - 1]++;
+            bResults[diceA.roll() + diceB.roll() - 2]++;
         }
+
         System.out.println("5-Sided Dice Rolled " + N2 + " Times");
         for (int i = 0; i < aResults.length; i++) {
             System.out.println((i + 1) + ": " + String.format("%04.3f", (((float) (aResults[i]) / N2) * 100)) + "% || "
                     + aResults[i] + " Times");
-        }
-
-        Dice diceB = new Dice(5);
-        int[] bResults = new int[9];
-        for (int i = 0; i < N2; i++) {
-            bResults[diceA.roll() + diceB.roll() - 2]++;
         }
         System.out.println("\nTwo 5-Sided Dice Rolled " + N2 + " Times & Summed");
         for (int i = 0; i < bResults.length; i++) {
@@ -32,20 +30,17 @@ class Probabilities {
     public static void problem3() {
         Dice diceA = new Dice(new int[] { 1, 2, 3, 3, 4, 5, 5, 5 });
         int[] aResults = new int[5];
-        for (int i = 0; i < N3; i++) {
-            aResults[diceA.roll() - 1]++;
-        }
-
-        System.out.println("5-Sided Dice Rolled " + N3 + " Times");
-        for (int i = 0; i < aResults.length; i++) {
-            System.out.println((i + 1) + ": " + String.format("%04.3f", (((float) (aResults[i]) / N3) * 100)) + "% || "
-                    + aResults[i] + " Times");
-        }
-
         Dice diceB = new Dice(new int[] { 1, 2, 3, 3, 4, 5, 5, 5 });
         int[] bResults = new int[9];
         for (int i = 0; i < N3; i++) {
+            aResults[diceA.roll() - 1]++;
             bResults[diceA.roll() + diceB.roll() - 2]++;
+        }
+
+        System.out.println("8-Sided Dice Rolled " + N3 + " Times");
+        for (int i = 0; i < aResults.length; i++) {
+            System.out.println((i + 1) + ": " + String.format("%04.3f", (((float) (aResults[i]) / N3) * 100)) + "% || "
+                    + aResults[i] + " Times");
         }
 
         System.out.println("\nTwo 8-Sided Dice Rolled " + N3 + " Times & Summed");
@@ -68,25 +63,20 @@ class Probabilities {
                 for (int q = j + 1; q < theDice.length; q++) {
                     int roll1 = theDice[j].roll();
                     int roll2 = theDice[q].roll();
-                    int increment = 0;
                     if (roll1 > roll2) {
-                        increment = 1;
-                    } else if (roll2 > roll1) {
-                        increment = -1;
+                        results[j][q]++;
+                    } else if(roll2 > roll1){
+                        results[q][j]++;
                     }
-                    results[j][q] += increment;
-                    results[q][j] -= increment;
-                    System.out.println("R" + j + ": " + roll1 + " R" + q + ": " + roll2 + "   Incrementing " + j + " "
-                            + q + " by " + increment + "   Decrementing " + q + " " + j + " by " + increment);
                 }
             }
         }
 
-        System.out.println("     1:  2:  3:  4:");
+        System.out.println("D1: D2:1:       2:       3:      4:");
         for (int i = 0; i < results.length; i++) {
             System.out.print((i + 1) + ":");
             for (int j = 0; j < results[i].length; j++) {
-                System.out.print("   " + results[i][j]);
+                System.out.print("   " + String.format("%.2f", ((float) results[i][j] / N4) * 100) + "%");
             }
             System.out.print('\n');
         }
