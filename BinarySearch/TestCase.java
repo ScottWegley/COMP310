0d02c84404ca0b56c0cdd73ff7336e502b80a6ed
Scott Wegley <scbrwegley@protonmail.ch>
2023-02-07 15:54:55 -0800

    added debug statements

--- BinarySearch/TestCase.java
+++ BinarySearch/TestCase.java
@@ -2,17 +2,21 @@ package BinarySearch;
 
 public class TestCase {
 
-    private static int N = 1000000;
+    private static int N = 5;
     public static int count = 0;
 
     public static void main(String[] args) {
-        int[] myArr = new int[N];
-        for (int i = 0; i < myArr.length; i++) {
-            myArr[i] = i;
+        for (int q = 4; q < 20; q += 2) {
+            count = 0;
+            N = q;
+            int[] myArr = new int[N];
+            for (int i = 0; i < myArr.length; i++) {
+                myArr[i] = i;
+            }
+            for (int i = 0; i < myArr.length; i++) {
+                BinarySearch.run(myArr, i);
+            }
+            System.out.println("avg. comparisons for length " + N + ": " + (double) count / N);
         }
-        for (int i = 0; i < myArr.length; i++) {
-            BinarySearch.run(myArr, i);
-        }
-        System.out.println("avg. comparisons for length " + N + ": " + (double)count/N);
     }
 }
