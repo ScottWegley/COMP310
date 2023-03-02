f8855d49e6ec9654287035d5c35547b8c275f8ed
Scott Wegley <scbrwegley@protonmail.ch>
2023-03-02 13:29:06 -0800

    a 2nd improvement

--- Week7/Mar3Script.java
+++ Week7/Mar3Script.java
@@ -4,10 +4,11 @@ public class Mar3Script {
 
     public static double evalpoly(double[] coefs, double x){
         double result = 0;
-        double xPower = Math.pow(x, coefs.length - 1);
-        for (int i = coefs.length - 1; i >= 0; i--) {
-            result += coefs[i] * xPower;
-            xPower /= x;
+        double xPower = 1;
+        double degree = coefs.length - 1;
+        for (int i = 0; i <= degree; i++) {
+            result+= coefs[i] * xPower;
+            xPower *= x;
         }
         return result;
     }
