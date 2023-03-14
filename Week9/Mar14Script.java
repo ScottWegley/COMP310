f261ff18be337d525afa98902da8f1786515f486
Scott Wegley <scbrwegley@protonmail.ch>
2023-03-14 14:04:50 -0700

    lil notetaking

---
+++ Week9/Mar14Script.java
@@ -0,0 +1,17 @@
+package Week9;
+
+public class Mar14Script {
+    public static void main(String[] args) {
+        
+    }
+
+    /**
+     * The first coefficient must equal 1.
+     * The polynomial must be of a degree such that it
+     * matches for 2^k-1, i.e. it must be a degree that
+     * is one less than a power of 2.
+     */
+    public static double preProcessedCoefs(){
+        return 0;
+    }
+}
