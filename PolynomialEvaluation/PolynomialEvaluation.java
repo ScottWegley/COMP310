da10bd1ac9fb8fb6f87bc0f7e021bc2db7ddf439
Scott Wegley <scbrwegley@protonmail.ch>
2023-03-21 14:00:54 -0700

    function to evaluate polynomial using horners method

--- PolynomialEvaluation/PolynomialEvaluation.java
+++ PolynomialEvaluation/PolynomialEvaluation.java
@@ -15,3 +15,20 @@
         }
         return result;
     }
+
+    /**
+     * Evaluate a polynomial in the form of a1x^n+a2x^n-1...+ax^0
+     * 
+     * @param coeffs An array of the coefficients in order from greatest power to
+     *               least power (a1,a2,a3), including 0's for missing degrees.
+     * @param x      The value of x to solve for.
+     * @return The result when the specified x is plugged into the equation.
+     */
+    public static double hornersMethod(double[] coeffs, double x) {
+        double result = coeffs[0];
+        for (int i = coeffs.length - 2; i >= 0; i--) {
+            result *= x;
+            result += coeffs[coeffs.length - 1 - i];
+        }
+        return result;
+    }
