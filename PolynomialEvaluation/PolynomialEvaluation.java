d255a6223778ce303e632d3bd1b9800bceb22fa1
Scott Wegley <scbrwegley@protonmail.ch>
2023-03-21 14:02:05 -0700

    began function for preprocessed coeffs calculation currently the divisor is established and the quotient is the correct length

--- PolynomialEvaluation/PolynomialEvaluation.java
+++ PolynomialEvaluation/PolynomialEvaluation.java
@@ -36,5 +36,25 @@ public class PolynomialEvaluation {
         return result;
     }
 
+    /**
+     * Evaluate a polynomial in the form of a1x^n+a2x^n-1...+ax^0.  The highest degree must be one less than a power of two.
+     * @param coeffs An array of the coefficients in order from greatest power to least power (a1,a2,a3), including 0's for missing degrees.
+     * @param x The value of x to solve for.
+     * @return The result when the specified x is plugged into the equation.
+     */
+    public static double preProcessedCoeffs(double[] coeffs, double x){
+        double k = Math.log(coeffs.length)/Math.log(2);
+        double j = Math.pow(2,k-1);
+        double b = coeffs[(int) (j)] - 1;
+        System.out.println("x^" + j + " + " + b);
+        double[] divisor = new double[(int) (coeffs.length-j+1)];
+        divisor[0] = 1;
+        divisor[divisor.length-1] = b;
+        double[] quotient = new double[(int) (coeffs.length-j)];
+        
+        
+        System.out.println("a");
+        return b;
+    }
 
 }
