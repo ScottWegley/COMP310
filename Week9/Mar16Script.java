1ffa4f3f72ccc4c3b61e97a6ca8377c72a13d5cd
Scott Wegley <scbrwegley@protonmail.ch>
2023-03-21 13:59:36 -0700

    type changes for testing

--- Week9/Mar16Script.java
+++ Week9/Mar16Script.java
@@ -1,15 +1,15 @@
 package Week9;
 
 public class Mar16Script {
-    public static double[][] matmult(int[][] A, int[][] B) throws IllegalArgumentException {
-        if (A[0].length != B.length)
+    public static double[][] matmult(double[][] arr1, double[][] arr2) throws IllegalArgumentException {
+        if (arr1[0].length != arr2.length)
             throw new IllegalArgumentException();
-        double[][] result = new double[A.length][B[0].length];
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < B[i].length; j++) {
+        double[][] result = new double[arr1.length][arr2[0].length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[i].length; j++) {
                 double sum = 0;
-                for (int k = 0; k < B.length; k++) {
-                    sum += A[i][k] * B[k][j];
+                for (int k = 0; k < arr2.length; k++) {
+                    sum += arr1[i][k] * arr2[k][j];
                 }
                 result[i][j] = sum;
             }
@@ -18,8 +18,8 @@ public class Mar16Script {
     }
 
     public static void main(String[] args) {
-        int[][] arr1 = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
-        int[][] arr2 = { { 1, 5 }, { 2, 6 }, { 3, 7 }, { 4, 8 } };
+        double[][] arr1 = { { 2, -4, 6 }, { 6, -6, 6 }, { 4, 2, 2 } };
+        double[][] arr2 = { { -0.2, 1.0/6.0, 0.1 }, { 0.1, -1.0/6.0, 0.2 }, { 0.3, -1.0/6.0, 0.1 } };
         double[][] arr3 = matmult(arr1, arr2);
 
         for (int i = 0; i < arr3.length; i++) {
