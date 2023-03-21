fb2413a247bad25fe3d6fa2656ed853b9ed6b475
Scott Wegley <scbrwegley@protonmail.ch>
2023-03-21 13:59:56 -0700

    function for regular evaluation of polynomials

---
+++ PolynomialEvaluation/PolynomialEvaluation.java
@@ -0,0 +1,17 @@
+    /**
+     * Evaluate a polynomial in the form of a1x^n+a2x^n-1...+ax^0
+     * 
+     * @param coeffs An array of the coefficients in order from greatest power to
+     *               least power (a1,a2,a3), including 0's for missing degrees.
+     * @param x      The value of x to solve for.
+     * @return The result when the specified x is plugged into the equation.
+     */
+    public static double evaluate(double[] coeffs, double x) {
+        double result = coeffs[coeffs.length - 1] + coeffs[coeffs.length - 2] * x;
+        double xPower = x;
+        for (int i = 2; i < coeffs.length; i++) {
+            xPower *= x;
+            result += coeffs[coeffs.length - 1 - i] * xPower;
+        }
+        return result;
+    }
