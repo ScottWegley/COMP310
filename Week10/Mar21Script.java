598fbe2da15018cabcc6c4dc09d110adc46c9dc9
Scott Wegley <scbrwegley@protonmail.ch>
2023-03-22 11:24:14 -0700

    started maybe thinking about matrix inversion

---
+++ Week10/Mar21Script.java
@@ -0,0 +1,13 @@
+package Week10;
+
+public class Mar21Script {
+    
+
+    public static double[][] getInverse(double[][] in){
+        double[][] inverse = new double[in.length][in[0].length];
+    }
+
+    public static void operate(double arg2){
+
+    }
+}
