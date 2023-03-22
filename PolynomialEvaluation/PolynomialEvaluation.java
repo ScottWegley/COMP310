1733bb8a1216bc1a00f1d8df5786c956ecef2844
Scott Wegley <scbrwegley@protonmail.ch>
2023-03-22 11:24:31 -0700

    got loop working

--- PolynomialEvaluation/PolynomialEvaluation.java
+++ PolynomialEvaluation/PolynomialEvaluation.java
@@ -1,10 +1,28 @@
 package PolynomialEvaluation;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class PolynomialEvaluation {
 
     public static void main(String[] args) {
-        double[] poly = new double[] { 1, 4, 0, -8, 6, 9, 2, -3 };
-        
+        double[] regular = new double[201];
+        double[] poly = new double[] { 1, 4, 0, 8, 6, 9, 2, 3 };
+        for (int i = 0; i <= 200.0; i += 1) {
+            System.out.println(round(((i/5.0) - 20.0),2));
+            // regular[i] = evaluate(poly, i);
+            // regular[(int) Math.round((i+20) * 5)][1] = evaluate(poly, i);
+            // regular[(int) Math.round((i+20) * 5)][0] = i;
+        }
+        System.out.println('a');
+    }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+    
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 
     /**
@@ -15,8 +33,8 @@ public class PolynomialEvaluation {
      * @param x      The value of x to solve for.
      * @return The result when the specified x is plugged into the equation.
      */
-    public static double evaluate(double[] coeffs, double x) {
-        double result = coeffs[coeffs.length - 1] + coeffs[coeffs.length - 2] * x;
+    public static float evaluate(double[] coeffs, double x) {
+        float result = (float) (coeffs[coeffs.length - 1] + coeffs[coeffs.length - 2] * x);
         double xPower = x;
         for (int i = 2; i < coeffs.length; i++) {
             xPower *= x;
@@ -43,23 +61,26 @@ public class PolynomialEvaluation {
     }
 
     /**
-     * Evaluate a polynomial in the form of a1x^n+a2x^n-1...+ax^0.  The highest degree must be one less than a power of two.
-     * @param coeffs An array of the coefficients in order from greatest power to least power (a1,a2,a3), including 0's for missing degrees.
-     * @param x The value of x to solve for.
+     * Evaluate a polynomial in the form of a1x^n+a2x^n-1...+ax^0. The highest
+     * degree must be one less than a power of two.
+     * 
+     * @param coeffs An array of the coefficients in order from greatest power to
+     *               least power (a1,a2,a3), including 0's for missing degrees.
+     * @param x      The value of x to solve for.
      * @return The result when the specified x is plugged into the equation.
      */
     // public static double preProcessedCoeffs(double[] coeffs, double x){
-    //     double k = Math.log(coeffs.length)/Math.log(2);
-    //     double j = Math.pow(2,k-1);
-    //     double b = coeffs[(int) (j)] - 1;
-    //     System.out.println("x^" + j + " + " + b);
-    //     double[] divisor = new double[(int) (coeffs.length-j+1)];
-    //     divisor[0] = 1;
-    //     divisor[divisor.length-1] = b;
-    //     double[] quotient = new double[(int) (coeffs.length-j)];
-             
-    //     System.out.println("a");
-    //     return b;
+    // double k = Math.log(coeffs.length)/Math.log(2);
+    // double j = Math.pow(2,k-1);
+    // double b = coeffs[(int) (j)] - 1;
+    // System.out.println("x^" + j + " + " + b);
+    // double[] divisor = new double[(int) (coeffs.length-j+1)];
+    // divisor[0] = 1;
+    // divisor[divisor.length-1] = b;
+    // double[] quotient = new double[(int) (coeffs.length-j)];
+
+    // System.out.println("a");
+    // return b;
     // }
     // Will probably hopefully never use.
 
