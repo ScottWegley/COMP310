0e4c8985d6cedaec24e112a20e695f2c9c848bbe
scottwegley <scottwegley@gmail.com>
2023-03-22 12:44:09 -0700

    finished

--- PolynomialEvaluation/PolynomialEvaluation.java
+++ PolynomialEvaluation/PolynomialEvaluation.java
@@ -1,5 +1,7 @@
 package PolynomialEvaluation;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
@@ -11,13 +13,30 @@ public class PolynomialEvaluation {
         double[] poly = new double[] { 1, 4, 0, 8, 6, 9, 2, 3 }; // x^7 + 4x^6 + 0x^5 + 8x^4 + 6x^3 + 9x^2 + 2x + 3
         double[][] preProcessedPoly = new double[][] {{1,0,0,0,5},{1,0,1},{1,4},{-1,4},{1,0,1},{1,-11},{1,-26}};
         //(x^4 + 0x^3 + 0x^2 + 0x + 5) * [(x^2 + 0x + 1) * (x + 4) + (-x + 4)] + [(x^2 + 0x + 1) * (x - 11) + (x - 26)]
+
+        String regPath = "C:\\Code\\CSC310\\PolynomialEvaluation\\regData.csv";
+        String proPath = "C:\\Code\\CSC310\\PolynomialEvaluation\\proData.csv";
+
         try {
+
+            BufferedWriter regWriter = new BufferedWriter(new FileWriter(regPath));
+            BufferedWriter proWriter = new BufferedWriter(new FileWriter(proPath));
+
             for (int i = 0; i <= 200.0; i += 1) {
                 regular[i] = evaluate(poly, round(((i/5.0) - 20.0),2));
                 horners[i] = hornersMethod(preProcessedPoly[0], round(((i/5.0) - 20.0),2)) * (hornersMethod(preProcessedPoly[1], round(((i/5.0) - 20.0),2))*hornersMethod(preProcessedPoly[2], round(((i/5.0) - 20.0),2))+hornersMethod(preProcessedPoly[3], round(((i/5.0) - 20.0),2))) + (hornersMethod(preProcessedPoly[4], round(((i/5.0) - 20.0),2))*hornersMethod(preProcessedPoly[5], round(((i/5.0) - 20.0),2))+hornersMethod(preProcessedPoly[6], round(((i/5.0) - 20.0),2)));
+                regWriter.write(Double.toString(round(((i/5.0) - 20.0),2)) + ", " + Double.toString(regular[i]) + "\n");
+                proWriter.write(Double.toString(round(((i/5.0) - 20.0),2)) + ", " + Double.toString(horners[i]) + "\n");
+                System.out.print(evaluate(poly, round(((i/5.0) - 20.0),2)));
+                System.out.print("   |||   ");
+                System.out.print(hornersMethod(preProcessedPoly[0], round(((i/5.0) - 20.0),2)) * (hornersMethod(preProcessedPoly[1], round(((i/5.0) - 20.0),2))*hornersMethod(preProcessedPoly[2], round(((i/5.0) - 20.0),2))+hornersMethod(preProcessedPoly[3], round(((i/5.0) - 20.0),2))) + (hornersMethod(preProcessedPoly[4], round(((i/5.0) - 20.0),2))*hornersMethod(preProcessedPoly[5], round(((i/5.0) - 20.0),2))+hornersMethod(preProcessedPoly[6], round(((i/5.0) - 20.0),2))));
+                System.out.print("\n");
             }
+
+            regWriter.close();
+            proWriter.close();
         } catch (Exception e) {
-            // TODO: handle exception
+            System.out.println(e.getMessage());
         }
     }
 
@@ -63,29 +82,5 @@ public class PolynomialEvaluation {
         }
         return result;
     }
-
-    /**
-     * Evaluate a polynomial in the form of a1x^n+a2x^n-1...+ax^0. The highest
-     * degree must be one less than a power of two.
-     * 
-     * @param coeffs An array of the coefficients in order from greatest power to
-     *               least power (a1,a2,a3), including 0's for missing degrees.
-     * @param x      The value of x to solve for.
-     * @return The result when the specified x is plugged into the equation.
-     */
-    // public static double preProcessedCoeffs(double[] coeffs, double x){
-    // double k = Math.log(coeffs.length)/Math.log(2);
-    // double j = Math.pow(2,k-1);
-    // double b = coeffs[(int) (j)] - 1;
-    // System.out.println("x^" + j + " + " + b);
-    // double[] divisor = new double[(int) (coeffs.length-j+1)];
-    // divisor[0] = 1;
-    // divisor[divisor.length-1] = b;
-    // double[] quotient = new double[(int) (coeffs.length-j)];
-
-    // System.out.println("a");
-    // return b;
-    // }
-    // Will probably hopefully never use.
 
 }
