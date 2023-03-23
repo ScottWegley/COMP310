f8498ffa5e2fd9761e981c24e960fb04504ebae6
Scott Wegley <scbrwegley@protonmail.ch>
2023-03-23 13:41:08 -0700

    working code

--- Week10/Mar21Script.java
+++ Week10/Mar21Script.java
@@ -1,13 +1,80 @@
 package Week10;
 
 public class Mar21Script {
-    
 
-    public static double[][] getInverse(double[][] in){
-        double[][] inverse = new double[in.length][in[0].length];
+    public static double[][] getInverse(double[][] mat) throws IllegalArgumentException {
+        int n = mat.length;
+        if (n != mat[0].length)
+            throw new IllegalArgumentException("Matrix must be a square");
+        double[][] inverse = new double[n][n];
+        double[][] identity = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            identity[i][i] = 1;
+        }
+
+        for (int k = 0; k < n; k++) {
+            int pivotloc = k;
+            double pivot = mat[pivotloc][pivotloc];
+            try {
+                while (Math.abs(pivot) < 0.0000001) {
+                    pivot = mat[++pivotloc][pivotloc];
+                }
+                swapRows(mat, k, pivotloc);
+                swapRows(identity, k, pivotloc);
+            } catch (IndexOutOfBoundsException e) {
+                throw new IllegalArgumentException("Matrix must have a determinant");
+            }
+            for (int j = 0; j < n; j++) {
+                mat[k][j] /= pivot;
+                identity[k][j] /= pivot;
+            }
+            for (int i = k + 1; i < n; i++) {
+                double factor = mat[i][k];
+                for (int j = 0; j < n; j++) {
+                    mat[i][j] -= factor * mat[k][j];
+                    identity[i][j] -= factor * identity[k][j];
+                }
+            }
+        }
+
+        for (int k = n - 1; k > 0; k--) {
+            for (int i = k - 1; i >= 0; i--) {
+                double factor = mat[i][k];
+                for (int j = 0; j < n; j++) {
+                    mat[i][j] -= factor * mat[k][j];
+                    identity[i][j] -= factor * identity[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                inverse[i][j] = identity[i][j];
+            }
+        }
+        return inverse;
     }
 
-    public static void operate(double arg2){
+    private static void swapRows(double[][] A, int r1, int r2){
+        for (int i = 0; i < A[r1].length; i++) {
+            double temp = A[r1][i];
+            A[r1][i] = A[r2][i];
+            A[r2][i] = temp;
+        }
+    }
 
+    public static void main(String[] args) {
+        double[][] matrix = new double[][] { { 0, -4, 0 }, { 6, -6, 6 }, { 4, 2, 2 } };
+        double[][] inverse = getInverse(matrix);
+        for (int i = 0; i < inverse.length; i++) {
+            for (int j = 0; j < inverse.length; j++) {
+                if (inverse[i][j] > 0) {
+                    System.out.print(" ");
+                }
+                System.out.printf("%.4f", inverse[i][j]);
+                System.out.print("   ");
+            }
+            System.out.print("\n");
+        }
     }
 }
