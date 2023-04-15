6a9fc811691de5c580cd4997077093f822f77228
scottwegley <scottwegley@gmail.com>
2023-04-15 12:18:37 -0700

    documentation

--- GraphTraversal/Edge.java
+++ GraphTraversal/Edge.java
@@ -3,17 +3,30 @@ package GraphTraversal;
 public class Edge {
    
     public enum ConnectionType {
+        /** Represents an Edge where you can only traverse in one direction. */
         UNIDIRECTIONAL,
+        /** Represents an Edge where you can traverse in either direction. */
         BIDIRECTIONAL
     }
 
     private int privFromNode, privToNode;
     ConnectionType privType;
 
+    /**
+     * Creates a unidirectional edge between two nodes.
+     * @param _fromNode The node the edge originates from.
+     * @param _toNode The node the edge ends at.
+     */
     public Edge(int _fromNode, int _toNode){
         this(_fromNode,_toNode,ConnectionType.UNIDIRECTIONAL);
     }
 
+    /**
+     * Creates an edge of a specified type between two nodes. 
+     * @param _fromNode The node the edge originates from.
+     * @param _toNode The nodes the edge originates at.
+     * @param _type The type of the node, either {@link ConnectionType#UNIDIRECTIONAL} or {@link ConnectionType#BIDIRECTIONAL}.
+     */
     public Edge(int _fromNode, int _toNode, ConnectionType _type){
         privFromNode = _fromNode;
         privToNode = _toNode;
