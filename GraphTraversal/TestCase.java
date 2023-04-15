3c31d83b900d6f96b33db59ec9b736732b9b1212
scottwegley <scottwegley@gmail.com>
2023-04-15 15:25:15 -0700

    tested out adjacency list

--- GraphTraversal/TestCase.java
+++ GraphTraversal/TestCase.java
@@ -22,12 +22,32 @@ public class TestCase {
                 new Edge(5, 2)
         };
 
-        AdjacencyMatrix adj8_1a = new AdjacencyMatrix(fig8_1a);
-        AdjacencyMatrix adj8_1b = new AdjacencyMatrix(fig8_1b);
+        AdjacencyMatrix adjMat8_1a = new AdjacencyMatrix(fig8_1a);
+        AdjacencyMatrix adjMat8_1b = new AdjacencyMatrix(fig8_1b);
+
+        AdjacencyList adjList8_1a = new AdjacencyList(fig8_1a);
+        AdjacencyList adjList8_1b = new AdjacencyList(fig8_1b);
 
         System.out.println("Matrix for 8-1a");
-        AlgoTools.printMatrix(adj8_1a.matrix);
+        AlgoTools.printMatrix(adjMat8_1a.matrix);
         System.out.println("Matrix for 8-1b");
-        AlgoTools.printMatrix(adj8_1b.matrix);
+        AlgoTools.printMatrix(adjMat8_1b.matrix);
+
+        System.out.println("List for 8-1a");
+        for (int i = 0; i < adjList8_1a.list.length; i++) {
+            System.out.print((i+1) + ":  ");
+            for (int q : adjList8_1a.list[i]) {
+                System.out.print(q + ",  ");
+            }
+            System.out.print('\n');
+        }
+        System.out.println("List for 8-1b");
+        for (int i = 0; i < adjList8_1b.list.length; i++) {
+            System.out.print((i+1) + ":  ");
+            for (int q : adjList8_1b.list[i]) {
+                System.out.print(q + ",  ");
+            }
+            System.out.print('\n');
+        }
     }
 }
