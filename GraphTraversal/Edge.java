184bb39b9c1dcbea7dbe851eb63d653fc851d80e
scottwegley <scottwegley@gmail.com>
2023-04-15 11:41:06 -0700

    added two primary constructors

--- GraphTraversal/Edge.java
+++ GraphTraversal/Edge.java
@@ -1,11 +1,23 @@
 package GraphTraversal;
 
-public class Edge {
+public class Edge<T> {
    
     public enum ConnectionType {
         UNIDIRECTIONAL,
         BIDIRECTIONAL
     }
 
+    T fromNode, toNode;
+    ConnectionType type;
+
+    public Edge(T _fromNode, T _toNode){
+        this(_fromNode,_toNode,ConnectionType.UNIDIRECTIONAL);
+    }
+
+    public Edge(T _fromNode, T _toNode, ConnectionType _type){
+        fromNode = _fromNode;
+        toNode = _toNode;
+        type = _type;
+    }
     
 }
