f89a28ddc0738a54e4b58036997695ce83d47efe
scottwegley <scottwegley@gmail.com>
2023-04-15 12:19:10 -0700

    function to add an edge

--- GraphTraversal/AdjacencyMatrix.java
+++ GraphTraversal/AdjacencyMatrix.java
@@ -11,4 +11,14 @@ public class AdjacencyMatrix {
         matrix = new int[_N][_N];
         N = _N;
     }
+
+    public void add(Edge e) throws IllegalArgumentException{
+        if(Math.max(e.fromNode(), e.toNode()) > N){
+            throw new IllegalArgumentException("An adjacency matrix of size " + N + " cannot have an edge involving the ID " + Math.max(e.fromNode(), e.toNode()));
+        }
+        matrix[e.fromNode()][e.toNode()] = 1;
+        if(e.type() == ConnectionType.BIDIRECTIONAL){
+            matrix[e.toNode()][e.fromNode()] = 1;
+        }
+    }
 }
