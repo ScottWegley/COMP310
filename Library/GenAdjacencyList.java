b0217235c2ac2e6bbaea4ec8b66eab86642d39e1
Scott Wegley <scbrwegley@protonmail.ch>
2023-04-25 14:52:31 -0700

    Created a generic adjacency list

---
+++ Library/GenAdjacencyList.java
@@ -0,0 +1,30 @@
+package Library;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import Library.Edge.ConnectionType;
+
+public class GenAdjacencyList<T> {
+    public HashMap<Object,ArrayList<T>> map;
+
+    public void add(Edge<T> e){
+        if(!map.containsKey(e.fromNode())){
+            map.put(e.fromNode(), new ArrayList<>());
+        }
+        map.get(e.fromNode()).add(e.toNode());
+        if(e.type() == ConnectionType.BIDIRECTIONAL){
+            if(!map.containsKey(e.toNode())){
+                map.put(e.toNode(), new ArrayList<>());
+            }
+            map.get(e.toNode()).add(e.fromNode());
+        }
+    }
+
+    public GenAdjacencyList(Edge<T>[] eArr){
+        map = new HashMap<>();
+        for (Edge<T> e : eArr) {
+            add(e);
+        }
+    }
+}
